eefda80b3552e04303fd98a3a2074f5f5bbbd5e3
Fabiano Silos <gsilos@gmail.com>
2021-08-17 16:54:33 +0200

    Add slugify script

--- slugify.sh
+++ slugify.sh
@@ -1,1 +1,2 @@
-echo "RECEIVED=$1" >> $GITHUB_ENV
+echo SLUGIFY_OUTPUT=$(echo $1 | iconv -t ascii//TRANSLIT | sed -r s/[~\^]+//g | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z) >> >> $GITHUB_ENV
+
